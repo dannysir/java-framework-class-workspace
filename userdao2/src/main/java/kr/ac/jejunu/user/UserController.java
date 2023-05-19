9177ed5a61ec99b553649eb625453641fb7cef92
Seo San <dannysir@naver.com>
2023-05-19 16:59:17 +0900

    1st

---
+++ userdao2/src/main/java/kr/ac/jejunu/user/UserController.java
@@ -0,0 +1,19 @@
+package kr.ac.jejunu.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping
+@RequiredArgsConstructor
+public class UserController {
+    private final UserDao userDao;
+
+    @RequestMapping("/user")
+    public User getUser(@RequestParam Long id) {
+        return userDao.findById(id);
+    }
+}
--- userdao2/src/main/java/kr/ac/jejunu/user/UserServlet.java
+++ userdao2/src/main/java/kr/ac/jejunu/user/UserServlet.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import java.io.IOException;
 
+
 public class UserServlet extends GenericServlet {
     private UserDao userDao;
 
